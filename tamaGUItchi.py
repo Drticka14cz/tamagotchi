395768eea4381b581d69e5311659e194a21b6379
Jakub Štěpánek <jakub14step@seznam.cz>
2026-01-06 15:37:09 +0100

    6.1.

--- tamaGUItchi.py
+++ tamaGUItchi.py
@@ -7,6 +7,7 @@ from PIL import Image
 # global cas_hladu
 # cas_hladu = dt.datetime.now() + dt.timedelta(seconds=10)
 
+
 zprava = None
 obrazek = None
 spritesheet = Image.open("cat.png")
@@ -64,6 +65,7 @@ def reset():
 
 }
     save_data()
+    zprava.text = "Reset dokončen"
 
 def status():
     # for i in beta:
@@ -74,12 +76,20 @@ Hlad je {beta['hlad']}
 Energie je {beta['energie']}
 
 """)#dopsat podmínku
+    zprava.content = f"""
+Hlad je {beta['hlad']}<br>
+Žízeň je {beta['žízeň']}<br>
+Energie je {beta['energie']}
+
+"""
+    
 
 def hra():
     beta["nešťastnost"] = False
     beta["energie"] -= 20
     beta["žízeň"] += 20
     beta["hlad"] += 20
+    zprava.content = "Zahráno"
     print(f"Právě sis hrál s Adolfem a je šťastný. \nEnergie({beta['energie']})\nŽízeň({beta['žízeň']})\nHlad({beta['hlad']})")
 
 def spánek():
@@ -131,7 +141,10 @@ def vystrihni_obrazek(x, y):
     return spritesheet.crop((x, y, x+64, y+64))
 
 def main():
-    global zprava, obrazek
+    global obrazek, zprava
+    #
+
+
     print("Vítej")
     print("""  
       /\\
@@ -142,6 +155,9 @@ def main():
     `'
   """)
     print("Pro ukončení napišš konec\n")
+
+
+    #taky stare
     tlacitka = {
         "Nakmit": nakrmit,
         "Hrát": hra, 
@@ -150,18 +166,25 @@ def main():
         "reset": reset, 
     }
     with ui.element("div").classes("w-full h-screen flex items-center justify-center flex-col flex gap-5"):
-        zprava = ui.label("ahaaaoj")
+        # zprava = ui.label("ahaaaoj")
+        ui.label("nadpis").classes("text-4xl")
         
+        print(zprava)
         obrazek = ui.image(vystrihni_obrazek(2,4)).classes("h-32 w-32")
-        for jmeno, funkce in tlacitka.items():
-            ui.button(jmeno, on_click=funkce)
-    
+        with ui.element("div").classes("w-100 h-50 border-2 border-solid border-black grid grid-cols-3 gap-y- gap-x-2 place-items-center"):
+            for jmeno, funkce in tlacitka.items():
+                ui.button(jmeno, on_click=funkce).classes("w-30 h-20")
+        with ui.element("div").classes("w-100 h-25 border-2 border-black border-solid"):
+            with ui.element("div").classes("place-items-center"):
+                ui.label("Status").classes("text-lg underline")
+            with ui.element("div").classes("place-items-center"):
+                zprava = ui.markdown("výpis")
+                
     cas_hladu = dt.datetime.now() + dt.timedelta(seconds=10)
     cas_stari = dt.datetime.now() + dt.timedelta(minutes=1)
 
     beta = load_data()
     # while True:
-    #    
     #     print("Looop")
     #     uziv_input = input("\n: ")
     #     aktual = dt.datetime.now()
@@ -189,6 +212,7 @@ def main():
 
             
     #     save_data()
+    #STARÉ - PRO KOMUNIKACI S TERMINÁLEM
     save_data()
     ui.run()
     
