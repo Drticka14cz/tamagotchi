269d083756fab683c023dcd3703944339eb8e386
Jakub Štěpánek <jakub14step@seznam.cz>
2026-01-13 15:52:33 +0100

    13.1

--- tamaGUItchi.py
+++ tamaGUItchi.py
@@ -65,7 +65,7 @@ def reset():
 
 }
     save_data()
-    zprava.text = "Reset dokončen"
+    zprava.content = "Reset dokončen"
 
 def status():
     # for i in beta:
@@ -79,7 +79,8 @@ Energie je {beta['energie']}
     zprava.content = f"""
 Hlad je {beta['hlad']}<br>
 Žízeň je {beta['žízeň']}<br>
-Energie je {beta['energie']}
+Energie je {beta['energie']}<br>
+Životy....? 
 
 """
     
@@ -89,13 +90,15 @@ def hra():
     beta["energie"] -= 20
     beta["žízeň"] += 20
     beta["hlad"] += 20
-    zprava.content = "Zahráno"
+    zprava.content = f"""Právě sis hrál s Adolfem a je šťastný. <br>Energie({beta['energie']}) <br>Žízeň({beta['žízeň']})<br>Hlad({beta['hlad']})"""
     print(f"Právě sis hrál s Adolfem a je šťastný. \nEnergie({beta['energie']})\nŽízeň({beta['žízeň']})\nHlad({beta['hlad']})")
 
 def spánek():
     beta["energie"] = 100
     print(f"Adolf se vyspal. \nEnergie({beta['energie']})")
+    zprava.content = f"Adolf se vyspal. \nEnergie({beta['energie']})"
     obrazek.source = vystrihni_obrazek(0,45)
+    zprava.content = f"Adolf se vyspal. \nEnergie({beta['energie']})"
 
 def nakrmit():
     
@@ -103,43 +106,54 @@ def nakrmit():
         beta["životy"] -=20
         beta["hlad"] -= 10
         print(f"odebral jsem 20 životů({beta['životy']}), \nodebral jsem 10 hladu{beta['hlad']}")
-        kontroluj_status()
+        
+        
         ui.notify("Nakrmeno")
+        kontroluj_status()
     else:
         beta["hlad"] -= 10
         if beta["životy"] < 100:
             beta["životy"] +=1
     print(f"Adolfův hlad je:{beta["hlad"]}")
+    zprava.content = f"Nakrmil jsi adolfa! (hlad: {beta['hlad']})"
 
 def kontroluj_status():
     if beta["životy"] <= 0:
         beta["žije"] == False
         print("Adolf umřel!")
         
-        ui.shutdown()
+        #ui.shutdown()
         exit()
     if beta["věk"] > 5:
         beta["žije"] == False
         print("Adolf umřel")
         
-        ui.shutdown()
+        #ui.shutdown()
         exit()
         
 def hladoveni():
     beta["hlad"] += 10
     if beta["hlad"] >= 150:
         beta["životy"] -=20
-        print(f"odebral jsem 20 životů({beta['životy']})")
+        print(f"Adolf má hlad !!!!\nOdebral jsem 20 životů({beta['životy']})")
+        zprava.content= f"Adolf má hlad!!! <br>Odebral jsem 20 životů({beta['životy']})"
         kontroluj_status()
-    print(f"Adolf má hlad! - ({beta["hlad"]})")
+    if beta["hlad"] >100:
+
+        print(f"Adolf má hlad! - ({beta["hlad"]})")
+        zprava.content = f"Adolf má hlad! - ({beta["hlad"]})"
+    else:
+        zprava.content = f"Adolf tráví své jídlo..."
 def starnuti():
-    beta["věk"] += 0
+    beta["věk"] += 1
+    zprava.content = f"Adolf mí narozky! je mu {beta["věk"]}!"
 
 def vystrihni_obrazek(x, y):
     x= x*64
     y= y*64
     return spritesheet.crop((x, y, x+64, y+64))
-
+def random_event():
+    zprava.content = f"BLO blo "
 def main():
     global obrazek, zprava
     #
@@ -165,7 +179,11 @@ def main():
         "status": status, 
         "reset": reset, 
     }
-    with ui.element("div").classes("w-full h-screen flex items-center justify-center flex-col flex gap-5"):
+    ui.timer(3600.0, starnuti)
+    ui.timer(60.0, hladoveni)
+    ui.timer(25.0, random_event)
+    ui.query("body").style("background-color: skyblue")
+    with ui.element("div").classes("w-full h-screen flex items-center justify-center flex-col flex gap-5 ").style(" width: 100vw; height: 100vh; background-color: skyblue; "):
         # zprava = ui.label("ahaaaoj")
         ui.label("nadpis").classes("text-4xl")
         
@@ -174,14 +192,14 @@ def main():
         with ui.element("div").classes("w-100 h-50 border-2 border-solid border-black grid grid-cols-3 gap-y- gap-x-2 place-items-center"):
             for jmeno, funkce in tlacitka.items():
                 ui.button(jmeno, on_click=funkce).classes("w-30 h-20")
-        with ui.element("div").classes("w-100 h-25 border-2 border-black border-solid"):
+        with ui.element("div").classes("w-100 h-38 border-2 border-black border-solid"):
             with ui.element("div").classes("place-items-center"):
                 ui.label("Status").classes("text-lg underline")
             with ui.element("div").classes("place-items-center"):
-                zprava = ui.markdown("výpis")
+                zprava = ui.markdown("")
                 
-    cas_hladu = dt.datetime.now() + dt.timedelta(seconds=10)
-    cas_stari = dt.datetime.now() + dt.timedelta(minutes=1)
+    # cas_hladu = dt.datetime.now() + dt.timedelta(seconds=10)
+    # cas_stari = dt.datetime.now() + dt.timedelta(minutes=1)
 
     beta = load_data()
     # while True:
